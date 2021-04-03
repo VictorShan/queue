9c0c9a7bf69455e690b0977d1fc6ef4177ad4b72
Victor Shan <victorshanvs@gmail.com>
2021-04-03 01:45:08 -0700

    added firebase?

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import { FirebaseAppProvider } from 'reactfire'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <FirebaseAppProvider>
+      <Component {...pageProps} />
+    </FirebaseAppProvider>
+  )
 }
 
 export default MyApp

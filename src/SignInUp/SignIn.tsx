57478cb75d1b2c05f831b40c85da6a7bc74038dc
Victor Shan <victorshanvs@gmail.com>
2021-04-07 01:36:49 -0700

    basic sign up/sign in works

--- src/SignInUp/SignIn.tsx
+++ src/SignInUp/SignIn.tsx
@@ -4,17 +4,23 @@ import { useState } from 'react'
 import { useAuth } from "reactfire"
 import Form from "react-bootstrap/Form"
 import Button from "react-bootstrap/Button"
+import { FormEvent } from 'react'
 
 export default function SignIn() {
     const auth = useAuth()
     const [email, setEmail] = useState<string | undefined>()
     const [password, setPassword] = useState<string | undefined>()
+    const [valid, setValid] = useState<boolean>(true)
+
+    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        const formData = new FormData(event.target as HTMLFormElement)
+        console.log(formData.get("email"), formData.get("password"))
+        auth.signInWithEmailAndPassword(email, password)
+    }
+
     return (
-        <Form onSubmit={e => {
-            e.preventDefault()
-            const formData = new FormData(e.target as HTMLFormElement)
-            console.log(formData.get("email"), formData.get("password"))
-        }}>
+        <Form onSubmit={handleSubmit}>
             <Form.Group>
                 <Form.Label>Email Address</Form.Label>
                 <Form.Control
--- src/SignInUp/SignUp.tsx
+++ src/SignInUp/SignUp.tsx
@@ -2,22 +2,42 @@ import 'firebase/auth'
 import { useAuth } from "reactfire"
 import Form from "react-bootstrap/Form"
 import Button from "react-bootstrap/Button"
+import { FormEvent } from 'react'
 
+// https://firebase.google.com/docs/auth/web/email-link-auth
 export default function SignUp() {
-
+    const auth = useAuth()
+    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        const formData = new FormData(event.target as HTMLFormElement)
+        console.log(formData.get("email"), formData.get("password"))
+        auth.createUserWithEmailAndPassword(
+            formData.get("email") as string,
+            formData.get("password") as string)
+            .catch(console.error)
+    }
     return (
-        <Form>
+        <Form onSubmit={handleSubmit}>
             <Form.Group>
                 <Form.Label>Email Address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Control
+                    name="email"
+                    type="email"
+                    placeholder="Enter email" />
             </Form.Group>
             <Form.Group>
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Enter password" />
+                <Form.Control
+                    name="password"
+                    type="password"
+                    placeholder="Enter password" />
             </Form.Group>
             <Form.Group>
                 <Form.Label>Confirm Password</Form.Label>
-                <Form.Control type="password" placeholder="Enter password" />
+                <Form.Control
+                    name="passwordConfirm"
+                    type="password"
+                    placeholder="Enter password" />
             </Form.Group>
             <Button type="submit">Submit</Button>
         </Form>
